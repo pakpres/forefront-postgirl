74e5bfd34e234922b59c7b748899e7466dd2672f
fakhripraya <fakhripraya.p@gmail.com>
2024-02-16 23:44:01 +0700

    fix versioning

--- src/routes/default.js
+++ src/routes/default.js
@@ -5,30 +5,27 @@ const {
 } = require("../variables/responseMessage");
 
 const defaultRoute = (app, mailer) => {
-  app.post(
-    `/v${process.env.APP_MAJOR_VERSION}/send`,
-    async (req, res) => {
-      // check query param availability
-      if (!req.body) return res.sendStatus(400);
-      if (!validateEmail(req.body.receiver))
-        return res.status(400).send(INVALID_EMAIL);
+  app.post(`/v1/send`, async (req, res) => {
+    // check query param availability
+    if (!req.body) return res.sendStatus(400);
+    if (!validateEmail(req.body.receiver))
+      return res.status(400).send(INVALID_EMAIL);
 
-      // run send function
-      const content = createMailContent(
-        req.body.receiver,
-        req.body.subject,
-        req.body.mailType,
-        req.body.props
-      );
-      mailer.sendMail(content, function (error, info) {
-        if (error) return res.status(400).send(error);
-        else
-          return res
-            .status(200)
-            .send("Email sent: " + info.response);
-      });
-    }
-  );
+    // run send function
+    const content = createMailContent(
+      req.body.receiver,
+      req.body.subject,
+      req.body.mailType,
+      req.body.props
+    );
+    mailer.sendMail(content, function (error, info) {
+      if (error) return res.status(400).send(error);
+      else
+        return res
+          .status(200)
+          .send("Email sent: " + info.response);
+    });
+  });
 };
 
 module.exports = {
