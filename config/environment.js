a219a339aa09925d905a8e27c1ee7985e5e08539
Fakhri Prayatna <fakhripraya.p@gmail.com>
2024-09-13 17:24:59 +0700

    add prefix for reverse proxy

--- config/environment.js
+++ config/environment.js
@@ -5,6 +5,10 @@ require("dotenv").config({
 
 exports.APP_PORT = process.env.PORT || 8001;
 exports.APP_STATE = process.env.APP_STATE || "DEV";
+exports.APP_WITH_PREFIX =
+  process.env.APP_WITH_PREFIX === "true" ||
+  process.env.APP_WITH_PREFIX === "1";
+
 exports.APP_ORIGIN =
   process.env.APP_ORIGIN || "http://localhost:3000";
 exports.APP_EMAIL_USER = process.env.APP_EMAIL_USER;
@@ -13,7 +17,8 @@ exports.APP_EMAIL_PASS = process.env.APP_EMAIL_PASS;
 exports.APP_CERT_PATH = process.env.APP_CERT_PATH || "";
 exports.APP_KEY_PATH = process.env.APP_KEY_PATH || "";
 exports.APP_ENABLE_LOCAL_HTTPS =
-  process.env.APP_ENABLE_LOCAL_HTTPS || false;
+  process.env.APP_ENABLE_LOCAL_HTTPS === "true" ||
+  process.env.APP_ENABLE_LOCAL_HTTPS === "1";
 
 exports.APP_OLYMPUS_SERVICE_BASE_URL =
   process.env.OLYMPUS_SERVICE_BASE_URL ||
--- index.js
+++ index.js
@@ -4,16 +4,22 @@ const {
   AppConfig,
   InitNodeMailer,
 } = require("./src/config");
+const { APP_WITH_PREFIX } = require("./config/environment");
 const expressApp = express();
 
 // Init App configurations
 const { server, app } = AppConfig(expressApp, express);
 
+// Define Router
+const routes = express.Router();
+
 // Init mailer
 const mailer = InitNodeMailer();
 
 // Init Routes
-defaultRoute(app, mailer);
+defaultRoute(routes, mailer);
+
+if (APP_WITH_PREFIX) app.use("/pg", routes);
 
 // Server listen
 const port = process.env.PORT || 6969;
