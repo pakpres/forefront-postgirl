6486b50d580f90f8cb2bbdf210550abc8ba7c037
Fakhri Prayatna <fakhripraya.p@gmail.com>
2024-11-17 20:02:39 +0700

    tidy up env config

--- config/environment.js
+++ config/environment.js
@@ -5,21 +5,21 @@ require("dotenv").config({
 
 exports.APP_PORT = process.env.PORT || 6000;
 exports.APP_STATE = process.env.APP_STATE || "DEV";
+exports.APP_API_KEY = process.env.APP_API_KEY || "";
 exports.APP_WITH_PREFIX =
   process.env.APP_WITH_PREFIX === "true" ||
   process.env.APP_WITH_PREFIX === "1";
 
-exports.APP_ORIGIN =
-  process.env.APP_ORIGIN || "http://localhost:3000";
 exports.APP_EMAIL_USER = process.env.APP_EMAIL_USER;
 exports.APP_EMAIL_PASS = process.env.APP_EMAIL_PASS;
-
 exports.APP_CERT_PATH = process.env.APP_CERT_PATH || "";
 exports.APP_KEY_PATH = process.env.APP_KEY_PATH || "";
 exports.APP_ENABLE_LOCAL_HTTPS =
   process.env.APP_ENABLE_LOCAL_HTTPS === "true" ||
   process.env.APP_ENABLE_LOCAL_HTTPS === "1";
 
+exports.APP_ORIGIN =
+  process.env.APP_ORIGIN || "http://localhost:3000";
 exports.APP_OLYMPUS_SERVICE_BASE_URL =
   process.env.OLYMPUS_SERVICE_BASE_URL ||
   "http://localhost:8000";
